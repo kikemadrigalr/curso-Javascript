ff7d5efc8b67c6aa6260d4db3e37fa4c21cde16a
Carlos Madrigal <carlosmad03@gmail.com>
2020-09-24 23:45:39 -0500

    recorriendo el dom

--- dom/dom.js
+++ dom/dom.js
@@ -106,6 +106,9 @@ console.log(document.scripts);
 
 // console.log($linkDom.hasAttribute("data-id"));
 
+//****************************************************************************** */
+/*
+
 // Manejo de estilos css desde JS
 const $linkDom = document.querySelector(".link-dom");
 
@@ -170,3 +173,161 @@ varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
 
 //asigno el nuevo valor a la etiqueta body
 $body.style.setProperty("background-color", varDarkColor);
+
+*/
+
+//**************************************************************************************** */
+//acceder al atributo class
+/*
+const $card = document.querySelector(".card");
+
+console.log($card);
+
+//className, devuelve la cadena de texto con el nombre de la clase
+
+console.log($card.className);
+
+//classList devuelve un token list con los valores del atributo class
+console.log($card.classList);
+
+//contains sirve para evaluar si un elemento contiene una clase en particular
+//devuelve true o false
+console.log($card.classList.contains("rotate-45"));
+
+//añadir una clase a un elemento con el metodo add de classist
+$card.classList.add("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+
+console.log($card.className);
+console.log($card.classList);
+
+//elimminar una clase de un elemento
+$card.classList.remove("rotate-45");
+
+//metodo toogle evalua si el elemento tiee la clase se la quita
+//si no la tiene, la coloca
+
+//como se elimino con el remove, el toogle coloca nuevaente la clase
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+
+//como el toogle anterior la coloca este la quita
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+
+$card.classList.toggle("rotate-45");
+
+//metodo replace, reemplaza una clase por otra, recibe dos parametros,
+// el primer parametro la clase se va a reemplazar y el segundo la clase que la reemplaza
+$card.classList.replace("rotate-45", "rotate-135");
+console.log($card.classList.contains("rotate-45"));
+
+//agregar, quitar o intercarmbiar variaas clases al mismo tiempo
+//usando el metodo add, separando cada clase con comas
+$card.classList.add("opcity-80", "sepia");
+
+//remove funciona tambien con mas de una clase a la vez
+$card.classList.remove("opcity-80", "sepia");
+
+//toogle tambien puede recibir mas de una clase a la vez
+$card.classList.toggle("opcity-80", "sepia");
+
+const $cards = document.querySelectorAll(".card");
+$cards.forEach((card) => card.classList.add("opcity-80", "sepia"));
+// console.log($cards);
+
+*/
+
+//****************************************************************************** */
+//interactual con tetxo en el DOM
+
+const $whatIsDom = document.getElementById("que-es");
+
+let text = `
+    <p>
+    El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model </i></b>) es un                    
+API para documentos HTML y XML.
+    </p>
+    <p>
+    Éste provée una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
+    </p>
+    <p>
+        <mark> El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.</mark>
+    </p>
+`;
+
+//agregar contenido de texto a un elemmento
+
+//inner text sirve para agregar contenido textual a un elemento del dom
+//se creo para internet explorer, funciona en cualquier navegador pero no es parte del standar
+//las etiquetas html las mostraria como cadenas de texto
+// $whatIsDom.innerText = text;
+
+//textContent sirve para agregar contenid textual a un elemento html
+//esta si forma parte del standar
+//las etiquetas html que contiene el texto seran mostradas como texto
+//esta es util solo para mostrar texto
+$whatIsDom.textContent = text;
+
+//se debe usar InnerHTML que, es para agregar contenido html dentro de un elemento del dom
+//esta es util para mostrar contenido html
+$whatIsDom.innerHTML = text;
+
+//mientras que los metodos anteriores agregan contenido dentro del elemento del dom
+//que se enuentra en la variable, el metodo outerHTML reemplaza por completo ese elemento
+//por el contenido html que le estemos pasando
+$whatIsDom.outerHTML = text;
+
+//********************************************************** */
+//Curso JavaScript: 67. DOM Traversing: Recorriendo el DOM
+//*********************************************************** */
+
+//utilizo este elemento para apartir de el ir recorriendo
+const $cards = document.querySelector(".cards");
+console.log($cards);
+
+//si quiero llegar a los hijos del elemento cards se utiliza la propiedad children
+//esta devuelve una coleccion html con todos los elementos hijos del elemento,
+// en este caso, los hijos de $cards
+console.log($cards.children);
+
+//con la notaion de los corchetes puedo acceder a un hijo en especifico
+console.log($cards.children[2]);
+
+//acceder al padre del elemento
+//ambas proiedades retornan el elemento padre
+console.log($cards.parentElement);
+console.log($cards.parentNode);
+
+//obtener el primer y el ultimo elemento hijo
+
+//firstChild devuelve el primer NODO hijo, sin importar el tipo
+console.log($cards.firstChild);
+
+// firstElementChild devuelve el primer ELEMENTO hijo
+console.log($cards.firstElementChild);
+
+//devuelve el ultimo NODO hijo del elemento sin importar el tipo de nodo
+console.log($cards.lastChild);
+
+//devuelve el ultim elemento hijo
+console.log($cards.lastElementChild);
+
+//acceder a elemento en el mismo nivel, es decir elementos hermanos
+
+//acceder al NODO hermano anterior, sin importar el tpo de nodo
+console.log($cards.previousSibling);
+
+//acceder al ELEMENTO hermano Anterior
+console.log($cards.previousElementSibling);
+
+//acceder al NODO hermano anterior, sin importar el tipo de nodo
+console.log($cards.nextSibling);
+
+//acceder al ELEMENTO hermano Siguiente
+console.log($cards.nextElementSibling);
+
+//closest obtiene el padre mas cercano del selector que se le pase como parametro
+console.log($cards.closest("div"));
+console.log($cards.closest("body"));
+console.log($cards.children[3].closest("section"));
